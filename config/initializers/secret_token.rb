1ccad939fc97f8c6fbe0cd248c2087c721b0b415
Daniel Wirr <daniel@metalplei.com>
2012-03-07 12:23:32 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lims::Application.config.secret_token = 'df0cc85a6bf5741407df7cc7588ec0f924d4f7d8a154e423ff0f8e91ad3fb147a154b0b420ce687727adbb423071173cc2446942fa179465fa91a33efc7a85af'
